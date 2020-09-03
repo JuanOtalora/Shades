ac44e2cefeb695c3f1359edbacee9c28c402615f
Juan Otalaora <juanpocsc@gmail.com>
2020-09-03 15:42:35 -0500

    Added basic mechanics

---
+++ Assets/Scripts/OpossumController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpossumController : MonoBehaviour
+{
+
+    public float speed = 4.0f;
+    private bool facingLeft = true;
+    public Vector2 movement;
+    Vector3 characterMovement;
+    public int x = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        CharacterMovement(x);
+    }
+
+    void CharacterMovement(float h)
+    {
+        characterMovement = new Vector3(h, 0f, 0f);
+        this.transform.position += characterMovement * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("EntraRRRR");
+        if (other.gameObject.transform.tag == "Obstacle")
+        {
+            //Do what you want when it collided with the ground
+            Debug.Log("Entra");
+            Flip();
+        }
+        else
+        {
+
+        }
+    }
+
+   
+
+
+    void Flip()
+    {
+        if (facingLeft && x == -1)
+        {
+            Vector3 y = new Vector3(0, 180, 0);
+            transform.Rotate(y);
+            facingLeft = false;
+            x = 1;
+        }else if (!facingLeft && x == 1)
+        {
+            Vector3 y = new Vector3(0, 180, 0);
+            transform.Rotate(y);
+            facingLeft = true;
+            x = -1;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  
+    Rigidbody2D rgb;
+    public float speed = 4.0f;
+    public Vector2 movement;
+    public Animator animatorC;
+    Vector3 characterMovement;
+    public float jumpForce = 400f;
+    public CircleCollider2D groundFox;
+    public bool grounded;
+    public BoxCollider2D headFox;
+    public bool isCrouched;
+    public bool facingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb = this.GetComponent<Rigidbody2D>();
+        facingRight = true;
+        groundFox = this.GetComponent<CircleCollider2D>();
+        headFox = this.GetComponent<BoxCollider2D>();
+        grounded = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Mathf.Abs(Mathf.Ceil(Input.GetAxis("Horizontal"))) > 0)
+        {
+            animatorC.SetFloat("Speed", 1);
+        }
+        else if (Input.GetAxis("Horizontal") == 0)
+        {
+            animatorC.SetFloat("Speed", 0);
+        }
+        DownWard();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.transform.tag == "Ground")
+        {
+            //Do what you want when it collided with the ground
+            animatorC.SetBool("Down", false);
+            grounded = true;
+        }
+        else if(other.gameObject.transform.tag == "Obstacle")
+        {
+            animatorC.SetBool("Down", false);
+            grounded = true;
+        }else if (other.gameObject.transform.tag == "Enemy")
+        {
+
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.transform.tag == "Ground")
+        {
+            //Do what you want when it collided with the ground
+            animatorC.SetBool("Down", false);
+            grounded = true;
+        }
+        else if (other.gameObject.transform.tag == "Obstacle")
+        {
+            animatorC.SetBool("Down", false);
+            grounded = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float HorizontalMovement = Input.GetAxis("Horizontal");
+        Jump();
+        if (Input.GetKey(KeyCode.DownArrow) && !isCrouched) {
+            Crouch();
+        }
+        else if(!Input.GetKey(KeyCode.DownArrow) && isCrouched)
+        {
+            headFox.enabled = true;
+            isCrouched = false;
+            animatorC.SetBool("Crouched", false);
+            speed = 4.0f;
+        }
+        Flip(HorizontalMovement);
+        CharacterMovement(Input.GetAxis("Horizontal"), Jump());
+
+    }
+
+    void Flip(float horizontal)
+    {
+        if (!facingRight && horizontal > 0)
+        {
+            Vector3 y = new Vector3(0, 180, 0);
+            transform.Rotate(y);
+            facingRight = true;
+        }
+        else if(facingRight && horizontal < 0)
+        {
+            Vector3 y = new Vector3(0, 180, 0);
+            transform.Rotate(y);
+            facingRight = false;
+        }
+       
+    }
+
+    void CharacterMovement(float h, float v)
+    {
+        characterMovement = new Vector3(h, v, 0f);
+        transform.position += characterMovement * Time.deltaTime * speed;
+    }
+
+    float Jump()
+    {
+        if (Input.GetButtonDown("Jump") && grounded) {
+            animatorC.SetBool("Jump", true);
+            rgb.AddForce(new Vector2(0f, jumpForce));
+            grounded = false;
+        }
+        return 0;
+    }
+
+    void DownWard()
+    {
+        if (rgb.velocity.y < 0)
+        {
+            animatorC.SetBool("Down", true);
+            animatorC.SetBool("Jump", false);
+        }
+    }
+
+
+    void Crouch()
+    {
+        speed = 2.0f;
+        headFox.enabled = false;
+        isCrouched = true;
+        animatorC.SetBool("Crouched", true);
+    }
+}
